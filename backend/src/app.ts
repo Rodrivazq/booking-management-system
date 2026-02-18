108a2bceeb619bde973aeca6e335ba6cc65ff8e8
Rodrigo Vazquez <rodrivazquez33@gmail.com>
2026-02-17 22:11:49 -0300

    Actualizacion de vercel

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,7 +12,16 @@ import statsRoutes from './routes/stats.routes';
 
 const app = express();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://booking-management-system-steel.vercel.app"
+    ],
+    credentials: true
+  })
+);
+
 app.use(express.json({ limit: '10mb' }));
 app.use(express.static(path.join(__dirname, '../public')));
 
